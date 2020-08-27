dc142f9e0f2bf7bfddec740890e4679f7e4d4fa1
Sree Sai Raghava <raghava.rym@gmail.com>
2020-08-27 07:29:23 +0530

    Initial Version

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
+import Test from "../views/Test.vue";
 
 Vue.use(VueRouter);
 
@@ -8,7 +9,7 @@ const routes = [
   {
     path: "/",
     name: "Home",
-    component: Home
+    component: Home,
   },
   {
     path: "/about",
@@ -17,14 +18,24 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
-  }
+      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+  },
+  {
+    path: "/test",
+    name: "Test",
+    component: Test,
+  },
+  {
+    // Catch all 404 - define at the very end
+    path: "*",
+    component: () => import("../views/NotFound.vue"),
+  },
 ];
 
 const router = new VueRouter({
   mode: "history",
   base: process.env.BASE_URL,
-  routes
+  routes,
 });
 
 export default router;
---
+++ src/views/NotFound.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="NotFound">
+    <v-container>
+      <v-layout row wrap>
+        <v-flex xs12>
+          <v-card height="400" color="transparennt" flat>
+            <div class="dispaly-3 mt-5">Page Not Found</div>
+            <div class="grey--text lighten-5">
+              The page you are trying to get never existed in this reality.
+              Or has migrated to a parallel universe ;)
+            </div>
+            <div class="paragraph-text mt-2">
+              <br />
+              <br />Try going back to home page and repeating your action. Or, contact helpdesk for support
+            </div>
+          </v-card>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.paragraph-text {
+  font-size: 18px;
+}
+</style>
